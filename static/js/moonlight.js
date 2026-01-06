939fcd3b01f73273f034fb9252db89029ef89c17
Arjun-Shukla <arjunhp14s@gmail.com>
2026-01-07 02:32:47 +0530

    added audio and fixed bugs

---
+++ static/js/moonlight.js
@@ -0,0 +1,7 @@
+let introaud = document.getElementById("introaud");
+// introaud.play();
+
+// let btn = document.getElementById("btn");
+window.addEventListener('DOMContentLoaded' , function plaf(){
+    introaud.play();
+});
